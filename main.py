509d1c809a695d38adbb9c7e7fb4555214dff0cd
Tanzeel Khan <ktanzeel80@gmail.com>
2021-03-23 23:21:05 +0530

    Modified code to show the number of ties.

--- main.py
+++ main.py
@@ -10,33 +10,38 @@ while i < 10:
     option = input()
     if option.lower() == "s" and choice == "snake":
         print("Tie")
-    if option.lower() == "s" and choice == "water":
+    elif option.lower() == "s" and choice == "water":
         print("You won this round.")
         player_score += 1
-    if option.lower() == "s" and choice == "gun":
+    elif option.lower() == "s" and choice == "gun":
         print("You lost this round.")
         comp_score += 1
-    if option.lower() == "w" and choice == "snake":
+    elif option.lower() == "w" and choice == "snake":
         print("You lost this round.")
         comp_score += 1
-    if option.lower() == "w" and choice == "water":
+    elif option.lower() == "w" and choice == "water":
         print("Tie")
-    if option.lower() == "w" and choice == "gun":
+    elif option.lower() == "w" and choice == "gun":
         print("You won this round.")
         player_score += 1
-    if option.lower() == "g" and choice == "snake":
+    elif option.lower() == "g" and choice == "snake":
         print("You won this round.")
         player_score += 1
-    if option.lower() == "g" and choice == "water":
+    elif option.lower() == "g" and choice == "water":
         print("You lost this round.")
         comp_score += 1
-    if option.lower() == "g" and choice == "gun":
+    elif option.lower() == "g" and choice == "gun":
         print("Tie")
-    i += 1
+    else:
+        print("Please enter a valid character.")
+    if option.lower() == "s" or  option.lower() == "w" or option.lower() == "g":
+        i += 1
+    else:
+        continue
 
 if player_score > comp_score:
-    print(f"You won the game with score {player_score} while computer's score was {comp_score}")
+    print(f"You won the game with score {player_score} while computer's score was {comp_score}. You had {10 - (player_score + comp_score)} ties.")
 elif comp_score > player_score:
-    print(f"You lost the game with score {player_score} while computer's score was {comp_score}")
+    print(f"You lost the game with score {player_score} while computer's score was {comp_score}. You had {10 - (comp_score + player_score)} ties.")
 else:
     print(f"This game was a tie with both scores equal to {player_score}.")
